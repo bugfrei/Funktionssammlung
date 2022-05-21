28775d63c965ea6b4566108afd1c03847188865a
bugfrei <cschle@gmail.com>
2022-05-21 04:08:58 +0200

    Initial

---
+++ Sammlung.cs
@@ -0,0 +1,19 @@
+public class Sammlung
+{
+    public void Funktion1
+    {
+        ;
+    }
+
+    //grepper Eine Funktion die das ganz toll kann
+    public void FunktionToll()
+    {
+        return null;
+    }
+    //grepper end
+
+    public void Funktion3
+    {
+        ;
+    }
+}
