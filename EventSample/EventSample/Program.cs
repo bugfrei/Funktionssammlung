8d1801411a6147351abf34e50e7bc4151c8eab48
bugfrei <cschle@gmail.com>
2022-05-21 05:57:49 +0200

    Events

---
+++ EventSample/EventSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSample
+{
+    //grepper Events in C# (EventHandler, event, RaiseEvent, Invoke Event)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventDemo ed = new EventDemo();
+            ed.MyEvent += Ed_MyEvent;
+
+            ed.RaiseEvent();
+
+            Console.ReadKey();
+        }
+
+        private static void Ed_MyEvent(object sender, string e)
+        {
+            Console.WriteLine($"Event ausgelöst {e}");
+        }
+    }
+
+
+    public class EventDemo
+    {
+        private EventHandler<string> MyEventHandler;
+        public event EventHandler<string> MyEvent;
+
+
+        public void RaiseEvent()
+        {
+            if (MyEvent != null)
+            {
+                MyEvent.Invoke(this, "Data");
+            }
+        }
+
+    }
+    //end grepper
+}
